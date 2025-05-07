e0e85bf2c47d8b6cf49188e185533e916401937b
Caleb Adebayo <calebtolorunleke@yahoo.com>
2025-05-07 22:28:00 +0100

    all good

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import "./App.css";
 // import Simplestate from "./components/Simplestate";
 // import LoginState from "./components/LoginState";
 // import { Conditional } from "./components/Conditional";
-import Activity1 from "./components/Activity1";
+// import Activity1 from "./components/Activity1";
+import Cart from "./components/cart/cart";
 
 function App() {
   // const [count, setCount] = useState(0);
@@ -13,7 +14,8 @@ function App() {
       {/* <Simplestate /> */}
       {/* <LoginState /> */}
       {/* <Conditional /> */}
-      <Activity1 />
+      {/* <Activity1 /> */}
+      <Cart />
     </>
   );
 }
---
+++ src/components/cart/Cart.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const cart = () => {
+  return (
+    <div>
+      <h1>aa</h1>
+    </div>
+  );
+};
+
+export default cart;
