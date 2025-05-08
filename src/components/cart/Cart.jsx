e74c333380b682eede244431eb48ed8d3c7a978e
Caleb Adebayo <calebtolorunleke@yahoo.com>
2025-05-08 22:11:06 +0100

    product page

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { products } from "./product";
 
 const cart = () => {
+  console.log(products);
   return (
-    <div>
-      <h1>aa</h1>
+    <div className="w-[420px] rounded-md shadow-2xl p-7 mx-auto ">
+      <h1 className="text-center text-4xl my-4">Your Bag</h1>
     </div>
   );
 };
---
+++ src/components/cart/product.jsx
@@ -0,0 +1,31 @@
+import deodorant from "/Users/caleb.adebayo/Documents/TechStudio/inDeReact/src/assets/deodorant.jpeg";
+import wristWatch from "/Users/caleb.adebayo/Documents/TechStudio/inDeReact/src/assets/wristWatch.jpg";
+import fans from "/Users/caleb.adebayo/Documents/TechStudio/inDeReact/src/assets/fans.jpeg";
+import phones from "/Users/caleb.adebayo/Documents/TechStudio/inDeReact/src/assets/phones.jpg";
+
+export const products = [
+  {
+    img: deodorant,
+    name: "Deodorant",
+    price: 70000,
+    id: 1,
+  },
+  {
+    img: wristWatch,
+    name: "Wrist Watch",
+    price: 50000,
+    id: 2,
+  },
+  {
+    img: fans,
+    name: "Fans",
+    price: 10000,
+    id: 3,
+  },
+  {
+    img: phones,
+    name: "Phones",
+    price: 30000,
+    id: 4,
+  },
+];
