cdc0bfc66cf02066a81b4e1d65a0767eb50786a9
Caleb Adebayo <calebtolorunleke@yahoo.com>
2025-05-09 22:44:04 +0100

    product page today

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -5,7 +5,7 @@ const cart = () => {
   console.log(products);
   return (
     <div className="w-[420px] rounded-md shadow-2xl p-7 mx-auto ">
-      <h1 className="text-center text-4xl my-4">Your Bag</h1>
+      <h1 className="text-center text-4xl p-5 my-4">Your Bag</h1>
     </div>
   );
 };
